00f0824da5f15eb73c4bf53a024e1500d1272682
RowDog <mrdiamonddirt@gmail.com>
2022-11-12 19:01:07 +0000

    🥇initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const  Section = styled.div`
+  background-color: red;
+  height: 100vh;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const PokeIMG = styled.div`
+background-color: azure;
+height: 100px;
+width: 100px;
+`
 
 function App() {
+  // useeffect for pokemon api call here
+  useEffect(() => {
+    let data = fetch('https://pokeapi.co/api/v2/pokemon/?limit=5000')
+    // parse json data from api call
+    .then(response => response.json())
+    // log data to console
+    .then(data => console.log(data))
+    console.log(data.length)
+    return () => {
+      
+    }
+  }, [])
+
+  const [pokemonnum, setpokemonnum] = useState(0)
+  let imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonnum}.png`
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Section>
+        <h1>Pokemons</h1>
+        <PokeIMG><img src={imgUrl} alt='pokeimg' ></img></PokeIMG>
+        
+        {/* input type range  with calue from 0 to 1154*/}
+        <input type='range' min='0' max='905' value={pokemonnum} onChange={(e) => setpokemonnum(e.target.value)}></input>
+
+        <input type='number' id='pokenum' placeholder='Pokenumber' value={pokemonnum}></input>
+        <button style={{background: 'blue',width:'40px', height:'40px'}} onClick={() => setpokemonnum(pokemonnum + 1)}>+</button> 
+        <button style={{background: 'blue',width:'40px', height:'40px'}} onClick={() => setpokemonnum(pokemonnum - 1)}>-</button>
+      </Section>
+      
     </div>
   );
 }
